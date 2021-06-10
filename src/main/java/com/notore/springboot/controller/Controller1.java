947c94b809e1ed533eb16328bc09a7943fe17bd8
FumiyaSumiyoshi <f.sumiyoshi@wix-net.jp>
2021-06-10 14:58:38 +0900

    html読み込み

--- src/main/java/com/notore/springboot/controller/Controller1.java
+++ src/main/java/com/notore/springboot/controller/Controller1.java
@@ -1,16 +1,16 @@
-package com.notore.springboot.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController//「Rest」(Representational State Transfer)専用のコントローラ
-public class Controller1 {
-	
-	@RequestMapping("/")
-	public String index () {
-		
-		return "Notore";
-		
-	}
-}
+//package com.notore.springboot.controller;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//@RestController//「Rest」(Representational State Transfer)専用のコントローラ
+//public class Controller1 {
+//	
+//	@RequestMapping("/")
+//	public String index () {
+//		
+//		return "Notore";
+//		
+//	}
+//}
--- src/main/java/com/notore/springboot/controller/Controller2.java
+++ src/main/java/com/notore/springboot/controller/Controller2.java
@@ -1,15 +1,15 @@
-//package com.notore.springboot.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class Controller2 {
-//	
-//	@RequestMapping("/")
-//	public String index () {
-//		
-//		return "index";
-//		
-//	}
-//}
+package com.notore.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Controller2 {
+	
+	@RequestMapping("/")
+	public String index () {
+		
+		return "index";
+		
+	}
+}
