299aff10d40a56f9d7615798c7415144e4ee7743
FumiyaSumiyoshi <f.sumiyoshi@wix-net.jp>
2021-06-13 19:22:53 +0900

    6.13 19:00

---
+++ src/main/java/com/notore/springboot/admin/controller/Controller1.java
@@ -0,0 +1,23 @@
+package com.notore.springboot.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Controller1 {
+	
+	@RequestMapping("/")
+	public String index () {
+		
+		return "index";
+		
+	}
+	
+	@GetMapping("/question")
+	public String questionlist () {
+		
+		return "questionlist";
+		
+	}
+}
