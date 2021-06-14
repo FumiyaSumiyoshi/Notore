8e2169a991bc8a5c16de0dcc25ee11797b9093b9
FumiyaSumiyoshi <f.sumiyoshi@wix-net.jp>
2021-06-14 12:29:13 +0900

    変更test

--- src/main/java/com/notore/springboot/admin/controller/Controller1.java
+++ src/main/java/com/notore/springboot/admin/controller/Controller1.java
@@ -1,23 +1,22 @@
-package com.notore.springboot.admin.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class Controller1 {
+//package com.notore.springboot.admin.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class Controller1 {
+//	
+//	@RequestMapping("/")
+//	public String index () {
+//		
+//		return "index";
+//		
+//	}
 	
-	@RequestMapping("/")
-	public String index () {
-		
-		return "index";
-		
-	}
-	
-	@GetMapping("/question")
-	public String questionlist () {
-		
-		return "questionlist";
-		
-	}
-}
+//	@GetMapping("/question")
+//	public String questionlist () {
+//		
+//		return "questionlist";
+//		
+//	}
+//}
