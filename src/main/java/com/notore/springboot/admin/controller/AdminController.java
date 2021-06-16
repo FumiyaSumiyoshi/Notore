f9d54f353dd5e4b8937f2f4dc05b92023e7f3386
FumiyaSumiyoshi <f.sumiyoshi@wix-net.jp>
2021-06-16 10:53:11 +0900

    notore.sqlを追加しました。

--- src/main/java/com/notore/springboot/admin/controller/AdminController.java
+++ src/main/java/com/notore/springboot/admin/controller/AdminController.java
@@ -6,11 +6,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 	
-	@RequestMapping("/admin")
-	public String index () {
+	@RequestMapping(value = "/admin")
+	public String index() {
 		
-		return "admin/index_top";
+		return "admin/questionlist";
 		
+	}
+	
+
+	@RequestMapping(value = "/admin2")
+	public String index2() {
+		
+		return "admin/categorylist";
+	}
+	
+	@RequestMapping(value = "/admin3")
+	public String index3() {
+		
+		return "admin/levellist";
+	}
+	
+	@RequestMapping(value = "/admin4")
+	public String index4() {
+		
+		return "admin/setquestion";
+	}
+	
+	@RequestMapping(value = "/admin5")
+	public String index5() {
+		
+		return "admin/user";
 	}
 	
 }
